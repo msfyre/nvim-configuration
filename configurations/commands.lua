ad7a5a2c6de1e45ab83b22163f2210b24bc2d1d3
MoonstoneFyre <moonstonefyre@gmail.com>
2025-12-06 13:20:24 +0800

    added sidebar command

--- configurations/commands.lua
+++ configurations/commands.lua
@@ -3,10 +3,22 @@ local commands = {
 		commandName = "RunCava",
 		commandDescription = "Runs the CAVA visualizer",
 		callback = function ()
-			vim.cmd("botright vnew");
 			vim.cmd("terminal powershell cava -p " .. cava_config)
 		end
-	}
+	},
+    Sidebar = {
+        commandName = "ToggleSidebar",
+        commandDescription = "Opens the sidebar of the client.",
+        callback = function ()
+            vim.cmd [[
+                " Initialize sidebar (terminal window)
+                botright new
+                terminal powershell
+            ]]
+            vim.cmd("belowright new");
+            vim.cmd("RunCava");
+        end
+    }
 }
 
 return commands
