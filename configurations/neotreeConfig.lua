3a9bc29ed0b1a6d70fed1684315854c773ce62c7
MoonstoneFyre <moonstonefyre@gmail.com>
2025-12-06 18:30:39 +0800

    plugin changes

---
+++ configurations/neotreeConfig.lua
@@ -0,0 +1,12 @@
+local neotreeConfig = {}
+
+neotreeConfig.filesystem = {
+	filtered_items = {
+		visible = true;
+	},
+	follow_current_file = {
+		enabled = true;
+	},
+}
+
+return neotreeConfig;
---
+++ configurations/neovimConfig.lua
@@ -0,0 +1,18 @@
+local neovimConfig = {}
+
+neovimConfig.interface = {
+	fullscreen = true;
+}
+
+neovimConfig.font = {
+	fontFace = "BigBlueTermPlus Nerd Font Mono",
+	fontSize = 10,
+}
+
+neovimConfig.editor = {
+    wrapLines = false,
+    tabSize = 4,
+    useSpaces = true
+}
+
+return neovimConfig
