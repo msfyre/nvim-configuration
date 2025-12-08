7a183ae3388626a07468f408345118713021f44a
MoonstoneFyre <moonstonefyre@gmail.com>
2025-12-08 19:54:49 +0800

    linted the notifications

--- lua/plugins/notifications.lua
+++ lua/plugins/notifications.lua
@@ -1,14 +1,14 @@
 return {
 	"rcarriga/nvim-notify",
-	config = function ()
-		local notifyPlugin = require("notify");
+	config = function()
+		local notifyPlugin = require("notify")
 
 		notifyPlugin.setup({
 			render = "default",
 			stages = "slide",
 			top_down = false,
-		});
+		})
 
-		vim.notify = notifyPlugin.notify;
-	end
+		vim.notify = notifyPlugin.notify
+	end,
 }
