f162d33f68225e439d6beac13411ed3f72287d71
MoonstoneFyre <moonstonefyre@gmail.com>
2025-12-08 13:40:37 +0800

    more hotkeys

--- lua/config/hotkeys.lua
+++ lua/config/hotkeys.lua
@@ -3,17 +3,54 @@ local hotkeys = {
 	mappings = {
 		normal = {
 			{
-				"<C-s>",
-				":w<CR>",
-				desc = "Save your progress.",
+				hotkey = "<C-S>",
+				action = function()
+					vim.cmd("w")
+					vim.notify("File saved!.", vim.log.levels.INFO, { title = "Saved!" })
+				end,
+				{
+					desc = "Save your progress",
+				},
 			},
 			{
-				"<C-S-s>",
-				":wa<CR>",
-				desc = "Save all open buffers.",
-			}
-		}
-	}
+				hotkey = "<C-V>",
+				action = function()
+					vim.cmd('"+P')
+				end,
+				{
+					desc = "Paste from the clipboard",
+				},
+			},
+			{
+				hotkey = "<leader>e",
+				action = function()
+					require("lua.modules.neotree").Toggle()
+				end,
+			},
+		},
+		visual = {
+			{
+				hotkey = "<C-c>",
+				action = function()
+					vim.cmd('"+y')
+				end,
+				{
+					desc = "Copy selected text.",
+				},
+			},
+		},
+		terminal = {
+			{
+				hotkey = "<Esc>",
+				action = function()
+					vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-N>", true, true, true), "t", false)
+				end,
+				{
+					desc = "Easily free yourself from a terminal window."
+				}
+			},
+		},
+	},
 }
 
 return hotkeys
