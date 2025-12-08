6916dbb36ff8769c370b81c4bcafb3e62e584809
MoonstoneFyre <moonstonefyre@gmail.com>
2025-12-08 15:13:37 +0800

    added color picker

--- lua/plugins/utilities.lua
+++ lua/plugins/utilities.lua
@@ -1,8 +1,13 @@
 return {
 	{
-		"nvim-treesitter/nvim-treesitter"
+		"nvim-treesitter/nvim-treesitter",
 	},
 	{
-		"nvim-telescope/telescope.nvim"
-	}
+		"nvim-telescope/telescope.nvim",
+	},
+	{
+		"uga-rosa/ccc.nvim",
+		event = { "BufEnter" }
+	},
 }
+
