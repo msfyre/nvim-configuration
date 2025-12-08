b9f0b8e04c0fa27a790264af1d01c6d97bbecfdd
MoonstoneFyre <moonstonefyre@gmail.com>
2025-12-08 15:14:31 +0800

    added intellisense for the cmdline

--- lua/plugins/LSP.lua
+++ lua/plugins/LSP.lua
@@ -144,4 +144,22 @@ return {
 			})
 		end,
 	},
+	{
+		"hrsh7th/cmp-cmdline",
+		dependencies = {
+			"hrsh7th/nvim-cmp",
+		},
+		config = function()
+			local cmp = require("cmp")
+
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" }, -- For file path completion
+				}, {
+					{ name = "cmdline" }, -- For Neovim command completion
+				}),
+			})
+		end,
+	},
 }
