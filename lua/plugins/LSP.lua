a746974b6cc3c8ca23dad75ed2d11ca001804eef
MoonstoneFyre <moonstonefyre@gmail.com>
2025-12-08 13:38:16 +0800

    configured the linter
    changed the events of it and auto-linted it

--- lua/plugins/LSP.lua
+++ lua/plugins/LSP.lua
@@ -10,45 +10,49 @@ return {
 		"mason-org/mason-lspconfig.nvim",
 		dependencies = {
 			"mason-org/mason.nvim",
-			"neovim/nvim-lspconfig" 
+			"neovim/nvim-lspconfig",
 		},
 		config = function()
-			require("mason-lspconfig").setup();
+			require("mason-lspconfig").setup()
 		end,
 	},
 	-- Linting & Formatting
 	{
 		"mfussenegger/nvim-lint",
-		event = { "BufReadPre", "BufNewFile" },
+		event = { "BufEnter", "BufWritePost" },
 		config = function()
 			local lint = require("lint")
 
 			lint.linters_by_ft = {
-				javascript = {'eslint'},
-				typescript = {'eslint'},
-				typescriptreact = {'eslint'},
-				markdown = {'markdownlint'},
+				javascript = { "eslint" },
+				typescript = { "eslint" },
+				typescriptreact = { "eslint" },
+				markdown = { "markdownlint" },
 			}
 
-			vim.api.nvim_create_autocmd({"BufWritePre"}, {
+			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 				callback = function()
 					lint.try_lint()
+
+					vim.notify("Lint complete.", vim.log.levels.INFO, {
+						title = "Linter",
+					})
 				end,
 			})
 		end,
 	},
 	{
 		"stevearc/conform.nvim",
-		event = { "BufWritePre" },
+		event = { "BufWritePost" },
 		config = function()
 			local conform = require("conform")
 
 			conform.setup({
 				formatters_by_ft = {
+					lua = { "stylua" },
 					javascript = { "prettierd" },
 					typescript = { "prettierd" },
 					typescriptreact = { "prettierd" },
-					markdown = { "prettierd" },
 				},
 				format_on_save = true,
 			})
@@ -64,16 +68,16 @@ return {
 			"saadparwaiz1/cmp_luasnip",
 			"L3MON4D3/LuaSnip", -- snippet engine
 			"onsails/lspkind.nvim",
-			"rafamadriz/friendly-snippets"
+			"rafamadriz/friendly-snippets",
 		},
 		config = function()
-			local cmp = require("cmp");
+			local cmp = require("cmp")
 
-			local luasnip = require("luasnip");
+			local luasnip = require("luasnip")
 
-			local lspkind = require("lspkind");
+			local lspkind = require("lspkind")
 
-			require("luasnip.loaders.from_vscode").lazy_load();
+			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
 				snippet = {
@@ -87,7 +91,7 @@ return {
 						mode = "symbol_text",
 						maxwidth = 50,
 						ellipsis_char = "...",
-					})
+					}),
 				},
 
 				window = {
@@ -128,6 +132,16 @@ return {
 					{ name = "buffer" },
 				}),
 			})
-		end
+
+			vim.lsp.config("lua_ls", {
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			})
+		end,
 	},
 }
