c45f3a4c85a54e94e715b236ab79a02c3b28e52e
MoonstoneFyre <moonstonefyre@gmail.com>
2025-12-15 19:40:20 +0800

    resolve

--- lua/events.lua
+++ lua/events.lua
@@ -1,16 +1,24 @@
 local Events = {}
 
-Events.VimEnter = function ()
-	local initMod = require("lua.modules.init");
+Events.VimEnter = function()
+	local initMod = require("lua.modules.init")
 
-	initMod.applyOverrides();
+	initMod.applyOverrides()
 
 	-- Boot lazy
 	require("config.lazy")
 
-	initMod.applyHotkeys();
-	initMod.applyConfigs();
-	initMod.applyTheme();
+	initMod.applyHotkeys()
+	initMod.applyConfigs()
+	initMod.applyTheme()
+end
+
+Events.BufWinEnter = function()
+	vim.cmd("silent! loadview")
+end
+
+Events.BufWinLeave = function()
+	vim.cmd("silent! mkview")
 end
 
 return Events
