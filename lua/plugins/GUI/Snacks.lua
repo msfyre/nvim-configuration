415a0f5fe76e68a5aa65d514c9292655c4910535
MoonstoneFyre <moonstonefyre@gmail.com>
2025-12-27 12:56:30 +0800

    configured snacks.nvim

---
+++ lua/plugins/GUI/Snacks.lua
@@ -0,0 +1,41 @@
+return {
+	"folke/snacks.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"nvim-telescope/telescope.nvim",
+	},
+	lazy = false,
+	priority = 1000,
+	opts = {
+		explorer = { enabled = true },
+		dashboard = { enabled = true },
+	},
+	keys = {
+		-- File Explorer
+		{
+			"<leader>e",
+			function()
+				Snacks.explorer()
+			end,
+			desc = "File Explorer",
+		},
+		-- Search
+		{
+			"<leader>ff",
+			function()
+				require("telescope.builtin").find_files({
+					hidden = true,
+					no_ignore = false,
+				})
+			end,
+			desc = "Find Files",
+		},
+		{
+			"<leader>f/",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "Grep",
+		},
+	},
+}
--- lua/plugins/GUI/init.lua
+++ lua/plugins/GUI/init.lua
@@ -1,11 +1,9 @@
 local loader = require("modules.Utilities.PluginLoader")
 
-local plugins = {
+return {
     loader.LoadPlugin("plugins.GUI.Themes"),
     loader.LoadPlugin("plugins.GUI.Notifications"),
-    loader.LoadPlugin("plugins.GUI.StatusLine"),
+    loader.LoadPlugin("plugins.GUI.Status Line"),
     loader.LoadPlugin("plugins.GUI.Snacks"),
     loader.LoadPlugin("plugins.GUI.Tabs"),
 }
-
-return plugins
